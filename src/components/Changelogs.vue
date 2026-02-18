0f2598709781a9afcd44fe9bb5a6d07563d6d0dd
leruduri <leo.tuduri@etu.uca.fr>
2026-02-18 01:01:07 +0100

    Fixed coupdecoeur page (normally)

--- src/components/Changelogs.vue
+++ src/components/Changelogs.vue
@@ -21,6 +21,14 @@ const cardListeners = {
 <template>
     <h2>Changelogs</h2>
     <article class="card" v-bind="cardAttrs" v-on="cardListeners">
+      <h3>V 0.2 : 17/06/2026</h3>
+        <div class="text">
+            <ul>
+                <li>Fix de la page Coup de coeur (j'espère : c'est une beta)</li>
+            </ul>
+        </div>
+    </article>
+    <article class="card" v-bind="cardAttrs" v-on="cardListeners">
       <h3>V 0.1 (beta) :17/02/2026</h3>
         <div class="text">
             <ul>
--- src/route.js
+++ src/route.js
@@ -1,14 +1,18 @@
 import { createWebHistory, createRouter } from 'vue-router';
 import LinksList from './components/LinksList.vue';
-import Home from './components/Home.vue'
+import Home from './components/Home.vue';
+import CoupCoeur from './components/CoupCoeur.vue';
+import Legal from './components/Legal.vue';
+import Changelogs from './components/Changelogs.vue';
+import NotFound from './components/NotFound.vue';
 
 const routes = [
   { name: 'home', path: '/', component: Home },
   { name: 'linktree', path: '/linktree', component: LinksList },
-  { name: 'coupdecoeur', path: '/coupdecoeur', component: () => import('./components/CoupCoeur.vue') },
-  { name: 'legal', path: '/mentions-legales', component: () => import('./components/Legal.vue') },
-  { name: 'changelogs', path: '/changelogs', component: () => import('./components/Changelogs.vue') },
-  { name: 'not-found', path: '/:pathMatch(.*)*', component: () => import('./components/NotFound.vue') },
+  { name: 'coupdecoeur', path: '/coupdecoeur', component: CoupCoeur },
+  { name: 'legal', path: '/mentions-legales', component: Legal },
+  { name: 'changelogs', path: '/changelogs', component: Changelogs },
+  { name: 'not-found', path: '/:pathMatch(.*)*', component: NotFound },
 ];
 
 export const router = createRouter({
